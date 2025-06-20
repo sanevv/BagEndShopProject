891d1110bce31367255665e9d8cfd3771f524590
sanhae.kang <sanevv@naver.com>
2025-06-20 11:33:54 +0900

    거슬리는거 변경

--- src/main/java/com/github/semiprojectshop/web/sanhae/test/HahaController.java
+++ src/main/java/com/github/semiprojectshop/web/sanhae/test/HahaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")//이게 만약 있다면 http://localhost:8080/am 이 공통적으로 들어가게된다.
-public class testController {
+public class HahaController {
 
     public String main() {
         return "index";//views 폴더가 루트고 그안의 jsp파일  jsp빼고 작성
