44b59b3863ed02d5e9931268c925990a0f4cc474
sirimp <sihu89@daum.net>
2025-07-01 17:46:29 +0900

    feat: 위시 조회 api생성

--- src/main/java/com/github/semiprojectshop/web/sihu/dto/product/wish/WishResponse.java
+++ src/main/java/com/github/semiprojectshop/web/sihu/dto/product/wish/WishResponse.java
@@ -3,14 +3,14 @@ package com.github.semiprojectshop.web.sihu.dto.product.wish;
 import com.github.semiprojectshop.config.DiscountConstants;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor(staticName = "of")
 public class WishResponse {
-    private final long productId;
-    private final String productName;
-    private final String productThumbnailUrl;
-    private final long productPrice;
+    private long productId;
+    private String productName;
+    private String productThumbnailUrl;
+    private long productPrice;
     public double getPriceAfterDiscount(){
         return Math.round(productPrice * (1-DiscountConstants.DISCOUNT_RATE));
     }
