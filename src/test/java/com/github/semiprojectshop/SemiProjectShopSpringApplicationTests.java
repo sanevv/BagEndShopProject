c42edc5cedbb7e74e9e5b1a19664a1f804d701e1
sanevv <sanevv@naver.com>
2025-09-16 14:43:08 +0900

    커밋 테스트2

--- src/test/java/com/github/semiprojectshop/SemiProjectShopSpringApplicationTests.java
+++ src/test/java/com/github/semiprojectshop/SemiProjectShopSpringApplicationTests.java
@@ -3,11 +3,8 @@ package com.github.semiprojectshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "jasypt.encryptor.password=bagend")
 class SemiProjectShopSpringApplicationTests {
-
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
