4f8025c69344d4cc471cacebb1de930f1906fa3d
sanevv <sanevv@naver.com>
2025-09-17 16:03:56 +0900

    메인페이지 공지사항 mainController 합침

--- src/main/java/com/github/semiprojectshop/web/sanhae/viewcontroller/MainController.java
+++ src/main/java/com/github/semiprojectshop/web/sanhae/viewcontroller/MainController.java
@@ -1,12 +1,33 @@
 package com.github.semiprojectshop.web.sanhae.viewcontroller;
 
+import com.github.semiprojectshop.repository.seungho.domain.NoticeVO;
+import com.github.semiprojectshop.repository.seungho.model.NoticeDAO;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 //웹 요청을 처리하는 컨트롤러
 @Controller
+@RequiredArgsConstructor
 public class MainController {
-    public String main() {
+
+    private final NoticeDAO ndao;
+
+
+    @GetMapping("/")
+    public String main(HttpServletRequest request) throws SQLException {
+        List<NoticeVO> nvoList = new ArrayList<>();
+        nvoList = ndao.selectMainPageNotice();
+
+        request.setAttribute("nvoList", nvoList);
+
         return "index";
     }
 }
